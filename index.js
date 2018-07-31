8dd0dd92d07e7e36f35dd819b6af2540378c5be0
jaycemxy <jayce_ma.xa@foxmail.com>
2018-07-31 13:59:38 +0800

    底部tab切换

--- index.js
+++ index.js
@@ -30,3 +30,23 @@ var Helpers = {
         return $node
     }
 }
+
+
+/* 底部tab切换 */
+var Paging = {
+    init: function () {
+        this.$tabs = $('footer>div')
+        this.$pages = $('main>section')
+        this.bind()
+    },
+    bind: function () {
+        var _this = this
+        this.$tabs.on('click', function () {
+            var $this = $(this)
+            var index = $this.index()
+            $this.addClass('active')
+                .siblings().removeClass('active')
+            _this.$pages.eq(index).fadeIn().siblings().fadeOut()
+        })
+    }
+}
