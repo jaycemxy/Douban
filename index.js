c92bad27f8afc4e00ee93938d5a2e62ebf9d0512
jaycemxy <jayce_ma.xa@foxmail.com>
2018-07-31 14:42:04 +0800

    finish us and search

--- index.js
+++ index.js
@@ -32,8 +32,13 @@ var Helpers = {
 }
 
 
-/* 底部tab切换 */
-var Paging = {
+$('footer>div').on('click', function(){
+    $(this).addClass('active').siblings().removeClass('active')
+    var index = $(this).index()
+    $('main section').eq(index).fadeIn().siblings().fadeOut()
+})
+
+/* var Paging = {
     init: function () {
         this.$tabs = $('footer>div')
         this.$pages = $('main>section')
@@ -49,7 +54,7 @@ var Paging = {
             _this.$pages.eq(index).fadeIn().siblings().fadeOut()
         })
     }
-}
+} */
 
 /* top250 */
 var Top250 = {
@@ -117,3 +122,117 @@ var Top250 = {
     }
 
 }
+
+var UsBoard = {
+    init: function () {
+        var _this = this
+        this.$container = $('#beimei')
+        this.$content = this.$container.find('.container')
+        this.getData(function (data) {
+            _this.renderData(data)
+        })
+    },
+    getData: function (callback) {
+        $.ajax({
+            url: '//api.douban.com/v2/movie/us_box',
+            data: {
+                start: 0,
+                count: 10
+            },
+            dataType: 'jsonp'
+        }).done(function (ret) {
+            callback(ret)
+        })
+    },
+    renderData(data) {
+        var _this = this
+        console.log(data)
+        data.subjects.forEach(function (item) {
+            var $node = Helpers.createNode(item.subject)
+            _this.$content.append($node)
+        })
+    }
+
+}
+
+var Search = {
+    init: function () {
+        this.page = 0
+        this.count = 10
+        this.isFinshed = false
+        this.isLoading = false
+        this.$container = $('#search')
+        this.$content = this.$container.find('.container')
+        this.bind()
+    },
+
+    bind: function () {
+        var _this = this
+        this.$container.find('.search-area .button').on('click', function () {
+            console.log('click ')
+            _this.getData(function (data) {
+                console.log(data)
+                _this.renderData(data)
+            })
+        })
+        console.log(this.$container.find('.search-area input'))
+        this.$container.find('.search-area input').on('keyup', function (e) {
+            if (e.key === 'Enter') {
+                _this.getData(function (data) {
+                    _this.renderData(data)
+                })
+            }
+        })
+
+        this.$container.on('scroll', function () {
+            console.log(_this.isLoading)
+            if (Helpers.isToBottom(_this.$container, _this.$container.find('.wrap')) && !_this.isFinshed && !_this.isLoading) {
+                console.log('to bottom')
+                _this.getData(function (data) {
+                    _this.renderData(data)
+                    _this.page++
+                        if (_this.page * _this.count > data.total) {
+                            _this.isFinshed = true
+                        }
+                })
+            }
+        })
+    },
+
+    getData: function (callback) {
+        var _this = this
+        var keyword = this.$container.find('.search-area input').val()
+        this.isLoading = true
+        $.ajax({
+            url: 'http://api.douban.com/v2/movie/search',
+            data: {
+                q: keyword
+            },
+            dataType: 'jsonp'
+        }).done(function (ret) {
+            _this.isLoading = false
+            callback(ret)
+        })
+    },
+
+    renderData(data) {
+        var _this = this
+        data.subjects.forEach(function (item) {
+            var $node = Helpers.createNode(item)
+            _this.$content.append($node)
+        })
+    }
+}
+
+
+
+
+var App = {
+    init: function () {
+        Top250.init()
+        UsBoard.init()
+        Search.init()
+    }
+}
+
+App.init()
